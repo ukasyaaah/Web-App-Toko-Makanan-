fe970988b88a82c64b9a7d1fd872d33964da6735
ukasyaaah <ukasyaaah@gmail.com>
2025-03-11 07:08:56 +0700

    Finished Callback Payment Gateway

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post('callback', App\Http\Controllers\Api\CallbackController::class);
